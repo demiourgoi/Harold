ba68cd68650077aef6341529b17311aae7c07efe
Juan Rodriguez Hortala <juanrh@pm.me>
2026-08-19 19:48:07 +0200

    Add `maude` dependency

--- harold-mcp/src/harold_mcp/server.py
+++ harold-mcp/src/harold_mcp/server.py
@@ -1,8 +1,14 @@
+import maude
 from fastmcp import FastMCP
+
+maude.init()
 
 mcp = FastMCP("Maude programming tools")
 
 
 @mcp.tool
 def greet(name: str) -> str:
-    return f"Hello, {name}!"
+    m = maude.getModule("NAT")
+    t = m.parseTerm("2 * 3")
+    t.reduce()
+    return f"Result = {t}"
